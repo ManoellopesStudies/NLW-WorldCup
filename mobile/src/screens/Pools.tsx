fad60ad2f48e508ba786f6d0f3dddefc10e5c3d3
Manoel Lopes <manoelvitorka@gmail.com>
2022-11-07 12:31:27 +0000

    Started with pools screen

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,9 +1,18 @@
-import { VStack } from "native-base";
+import { Octicons } from "@expo/vector-icons";
+import { Icon, VStack } from "native-base";
+import { Button } from "../components/Button";
+import { Header } from "../components/Header";
 
 export function Pools() {
   return (
     <VStack flex={1} bgColor="gray.900">
-
+      <Header title={"My Pools"}/>
+      <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
+        <Button 
+          title="SEARCH POOL BY CODE"
+          leftIcon={<Icon as={Octicons} name="search" color="black" size="md"/>}
+        />
+      </VStack>
     </VStack>
   )
 }
